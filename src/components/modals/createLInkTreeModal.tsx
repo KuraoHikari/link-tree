d7e89dae6d7088566da9a499a9e782867d312772
KuraoHikari <dewaindra705@gmail.com>
2023-10-25 12:04:57 +0800

    update modal and hooks privider

---
+++ src/components/modals/createLInkTreeModal.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const CreateLinkTreeModal = () => {
+ return <div></div>;
+};
---
+++ src/hooks/useModalStore.tsx
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+
+export type ModalType =
+ | "createLinkTree"
+ | "deleteLinkTree"
+ | "editLinkTree";
+
+interface ModalData {
+ linkTree?: {
+  title: string;
+  description: string;
+ };
+}
+
+interface ModalStore {
+ type: ModalType | null;
+ data: ModalData;
+ isOpen: boolean;
+ onOpen: (type: ModalType, data?: ModalData) => void;
+ onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+ type: null,
+ data: {},
+ isOpen: false,
+ onOpen: (type, data = {}) =>
+  set({ isOpen: true, type, data }),
+ onClose: () => set({ type: null, isOpen: false }),
+}));
